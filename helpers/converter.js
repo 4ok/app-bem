a49c628f72f7783a4621aeee97d968627a4d3c0d
lipolyakov <lipolyakov@yandex-team.ru>
2016-03-25 02:34:23 +0300

    Refactoring

--- helpers/converter.js
+++ helpers/converter.js
@@ -1,14 +1,20 @@
 'use strict';
 
-const Converter       = require('app-core/helpers/converter');
-const markdownBemjson = require('markdown-bemjson')();
+const Helper          = require('app-core/components/helper/abstract');
 const string          = require('underscore.string');
+const markdownBemjson = require('markdown-bemjson')();
+const crypto          = require('crypto');
 
 // TODO
 const bundleDir = '/Users/lipolyakov/development/projects/trusha/application/bem/bundles/index';
 const bemhtml = require(bundleDir + '/index.bemhtml.final.js').BEMHTML;
 
-module.exports = class extends Converter {
+module.exports = class extends Helper {
+
+    constructor(http) {
+        super(http);
+        this._cache = {};
+    }
 
     bemjsonToHtml(bemjson) {
         return bemhtml.apply(bemjson)
@@ -28,5 +34,37 @@ module.exports = class extends Converter {
         }
 
         return result;
+    }
+
+    markdownToBemjson(markdown) {
+        return markdownBemjson.convert(markdown);
+    }
+
+    markdownToText(markdown, length) {
+        const hash = this._getMethodParamsHash('markdownToText', arguments);
+
+        if (!this._cache[hash]) {
+            const bemjson = this.markdownToBemjson(markdown);
+
+            this._cache[hash] = this.bemjsonToText(bemjson, length);
+        }
+
+        return this._cache[hash];
+    }
+
+    _getMethodParamsHash(method, args) {
+        const params = Array.prototype.join.call(args, '|');
+        const str    = params
+            .map(param => (typeof param == 'object')
+                ? JSON.parse(param)
+                : param
+            )
+            .concat(method)
+            .join('~');
+
+        return crypto
+            .createHash('sha256')
+            .update(str)
+            .digest('hex');
     }
 };
