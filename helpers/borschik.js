90921d4dc44d266e6f24503f6a3a76f98f081d26
lipolyakov <lipolyakov@yandex-team.ru>
2017-10-16 06:08:41 +0300

    Fixed eslint

--- helpers/borschik.js
+++ helpers/borschik.js
@@ -4,28 +4,19 @@ const borschik = require('borschik/js/borschik');
 
 const PROJECT_LINKS_FILE = 'bem/links.json';
 
+const linksFile = this._projectDir + '/' + PROJECT_LINKS_FILE;
+
+if (fs.existsSync(linksFile)) {
+    // eslint-disable-next-line global-require, import/no-dynamic-require
+    const links = require(linksFile);
+
+    borschik.addLinks(links);
+}
+
 module.exports = class extends Helper {
 
-    constructor(...args) {
-        super(...args);
-        const linksFile = this._projectDir + '/' + PROJECT_LINKS_FILE;
-
-        if (fs.existsSync(linksFile)) {
-            this.addLinks(linksFile);
-        }
-    }
-
-    static get return() {
+    // eslint-disable-next-line class-methods-use-this
+    get return() {
         return borschik;
-    }
-
-    static addLinks(links) {
-
-        if (typeof links === 'string') {
-            // eslint-disable-next-line global-require, import/no-dynamic-require
-            links = require(links);
-        }
-
-        borschik.addLinks(links);
     }
 };
