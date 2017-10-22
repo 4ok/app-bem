9393e14edc6c41e2bedc7050ca3ae434654fd824
lipolyakov <lipolyakov@yandex-team.ru>
2017-10-23 01:18:41 +0300

    Fix a borschik helper

--- helpers/borschik.js
+++ helpers/borschik.js
@@ -1,19 +1,22 @@
 const Helper = require('app-core/components/helper/abstract');
 const fs = require('fs');
 const borschik = require('borschik/js/borschik');
-
 const PROJECT_LINKS_FILE = 'bem/links.json';
 
-const linksFile = this._projectDir + '/' + PROJECT_LINKS_FILE;
-
-if (fs.existsSync(linksFile)) {
-    // eslint-disable-next-line global-require, import/no-dynamic-require
-    const links = require(linksFile);
-
-    borschik.addLinks(links);
-}
-
 module.exports = class extends Helper {
+
+    constructor(...args) {
+        super(...args);
+
+        const linksFile = this._projectDir + '/' + PROJECT_LINKS_FILE;
+
+        if (fs.existsSync(linksFile)) {
+            // eslint-disable-next-line global-require, import/no-dynamic-require
+            const links = require(linksFile);
+
+            borschik.addLinks(links);
+        }
+    }
 
     // eslint-disable-next-line class-methods-use-this
     get return() {
